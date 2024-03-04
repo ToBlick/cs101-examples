3fd64943df85e2d473c5d310a033f7782c5790f6
Tobias Blickhan <tobias.blickhan@gmail.com>
2024-03-04 15:03:35 -0500

    fix snek bugs, add row-column-major example

---
+++ src/arrays/RowColumn.java
@@ -0,0 +1,41 @@
+package arrays;
+
+import java.util.Arrays;
+import java.util.Locale;
+
+public class RowColumn {
+
+    static void printTime(String name, double time) {
+        System.out.printf(Locale.US, "Time spent for " + name + ": \t %.2fms \n", time/1e6);
+    }
+
+    public static void main(String[] args) {
+        
+
+        int n = 10_000;
+        int m = 10_000;
+        int[][] myArray = new int[n][m];
+
+        long startTime = System.nanoTime();
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<m; j++) {
+                myArray[i][j] = 0;
+                
+            }
+        }
+        long endTime   = System.nanoTime();
+        long totalTime = endTime - startTime;
+        printTime("i then j", totalTime);
+
+
+        startTime = System.nanoTime();
+        for (int j=0; j<m; j++) {
+            for (int i=0; i<n; i++) {  
+                myArray[i][j] = 0;
+            }
+        }
+        endTime   = System.nanoTime();
+        totalTime = endTime - startTime;
+        printTime("j then i", totalTime);
+    }
+}
--- src/arrays/Snake.java
+++ src/arrays/Snake.java
@@ -1,36 +1,18 @@
 package arrays;
 
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 /**
- * The Snake class represents a simple snake board.
- * The board is played on a grid where the snake (represented by green squares) moves around and tries to eat the food (represented by yellow squares). After eating food, the snake grows by one square.
+ * The Snake class represents a simple snake game.
+ * The game is played on a grid where the snake (represented by green squares) moves around and tries to eat the food (represented by yellow squares).
  * The objective of the board is to eat as much food as possible without colliding with the boundaries of the grid or the snake's own body.
- * The board ends when the snake collides with a boundary or its own body.
- * 
- * The board is controlled by the user, who can make the snake move up, down, left, or right using the 'w', 'a', 's', and 'd' keys respectively.
- * 
- * The Snake class provides methods for initializing the board, printing the board state, updating the board state based on user input, and checking for win or loss conditions.
- * 
- * The board is played by calling the main method of the Snake class.
- * 
- * Example usage:
- * 
- * int[][] myBoard = new int[9][9];
- * Snake.initBoard(myBoard);
- * Snake.putFood(myBoard);
- * boolean validInput = true;
- * while (validInput) {
- *     Snake.printboard(myBoard);
- *     validInput = Snake.userMove(myBoard);
- *     if (Snake.checkWin(myBoard)) {
- *         System.out.println("You win! :)");
- *         return;
- *     }
- * }
- * System.out.println("You lose :(");
+ * The game ends when the snake collides with a boundary or its own body or when it fills up the entire board.
+ * The user can make the snake move up, down, left, or right using the 'w', 'a', 's', and 'd' keys respectively.
+ * Internally, the game is represented by a 2D array of integers. The snake is represented by positive integers that decrease by one from the snake's head to its tail. The food is represented by -1. The field is represented by 0. Whenever the snake moves, the cell in the direction it is moving becomes its new head with a value one greater than the previous head. After every move, the value of every snake-cell is decremented by one. If the value of a snake-cell becomes zero, the snake has moved off that cell. If the snake eats food, the value of every snake-cell is incremented by one, effectivelt growing it in length by one cell.
+ * The game is played by calling the main method of the Snake class.
  */
 public class Snake {
 
@@ -38,28 +20,27 @@ public class Snake {
     static Scanner in = new Scanner(System.in);
     static Random rng = new Random();
 
-
     /**
      * Print the board.
-     * Snake: Green Square:  🟩 (U+1F7E9)
-     * Food:  Yellow Square: 🟨 (U+1F7E8)
-     * Field: Black Square:  ⬛ (U+2B1B)
+     * Snake:       🟩 (U+1F7E9)
+     * Snake head:  👀 (U+1F440)
+     * Food:        🟨 (U+1F7E8)
+     * Field:       ⬛ (U+2B1B)
      * @param board
      */
     public static void printboard(int[][] board) {
+        int boardMax = boardMaximum(board);
         for (int[] row : board) {
             String toPrint = "";
             for (int cell : row) {
-                switch (cell) {
-                    case 0:     // Field
-                        toPrint += "⬛";
-                        break;
-                    case -1:    // Food
-                        toPrint += "🟨";
-                        break;
-                    default:    // Snake
-                        toPrint += "🟩";
-                        break;
+                if (cell == 0) { // Field
+                    toPrint += "⬛";     
+                } else if (cell == boardMax) { // Snake head
+                    toPrint += "👀";       
+                } else if (cell == -1) {// Food   
+                    toPrint += "🟨";     
+                } else { // Snake
+                    toPrint += "🟩";
                 }
             }
             printWriter.println(toPrint); // we need to use printWriter.println() instead of System.out.println() to avoid the Unicode characters being printed as question marks
@@ -70,11 +51,10 @@ public class Snake {
      * Initialize the board. Set all cells to 0 and place the snake at a random position.
      * @param board The board to initialize.
      */
-    @SuppressWarnings("unused")
     public static void initBoard(int[][] board) {
-        for (int[] row : board) {
-            for (int cell : row) {
-                cell = 0; // field
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                board[i][j] = 0; // note that we cannot iterate using (int cell : row) because we need to modify the cell _value_
             }
         }
         int i = rng.nextInt(board.length);
@@ -136,6 +116,18 @@ public class Snake {
         return position;
     }
 
+    public static int boardMaximum(int[][] board) {
+        int max = board[0][0];
+        for (int[] row : board) {
+            for (int cell : row) {
+                if (cell > max) {
+                    max = cell;
+                }
+            }
+        }
+        return max;
+    }
+
     /**
      * Check if the game is won.
      * @param board The board to check.
@@ -183,19 +175,24 @@ public class Snake {
     /**
      * Move the snake based on user input.
      * @param board The board the snake is on.
-     * @return true if the move is valid, false otherwise.
+     * @return true if the move is legal, false otherwise.
      */
     public static boolean userMove(int[][] board) {
         int[] oldPosition = snakeHead(board);
         int[] newPosition = oldPosition.clone();
-        System.out.println("Enter w, a, s, or d to move:");
-        String move = in.nextLine();
-        newPosition = calculateNewPosition(move, newPosition);
-        if (!isValidMove(newPosition, board)) {
-            return false;
+        boolean isValidInput = false;
+        String move = "w";
+        while (!isValidInput) { // input loop
+            System.out.println("Enter w, a, s, or d to move:");
+            move = in.nextLine();
+            isValidInput = validateInput(move);
         }
-        handleMove(newPosition, oldPosition, board);
-        return true;
+        newPosition = calculateNewPosition(move, newPosition, board);
+        return handleMove(newPosition, oldPosition, board);
+    }
+
+    public static boolean validateInput(String move) {
+        return move.equals("w") || move.equals("a") || move.equals("s") || move.equals("d");
     }
 
     /**
@@ -204,8 +201,8 @@ public class Snake {
      * @param currentPosition The current position of the snake's head as an array of two integers.
      * @return The new position of the snake's head as an array of two integers.
      */
-    public static int[] calculateNewPosition(String move, int[] currentPosition) {
-        int[] newPosition = currentPosition.clone();
+    public static int[] calculateNewPosition(String move, int[] currentPosition, int[][] board) {
+        int[] newPosition = Arrays.copyOf(currentPosition, 2);
         switch (move) {
             case "w":
                 newPosition[0]--; // move up
@@ -216,27 +213,13 @@ public class Snake {
             case "a":
                 newPosition[1]--; // move left
                 break;
-            case "d":
+            default: // "d"
                 newPosition[1]++; // move right
-                break;
-            default:
-                System.out.println("Invalid input, try again:");
                 break;
         }
         return newPosition;
     }
 
-    /**
-     * Check if the new position of the snake's head is valid, i.e. if it is within the boundaries of the board.
-     * @param newPosition The new position of the snake's head as an array of two integers.
-     * @param board The board the snake is on.
-     * @return true if the move is valid, false otherwise.
-     */
-    public static boolean isValidMove(int[] newPosition, int[][] board) {
-        int row = newPosition[0];
-        int col = newPosition[1];
-        return row >= 0 && row < board.length && col >= 0 && col < board[row].length;
-    }
 
     /**
      * Handle the move of the snake based on the new position of the snake's head.
@@ -244,24 +227,33 @@ public class Snake {
      * @param oldPosition The old position of the snake's head as an array of two integers.
      * @param board The board the snake is on.
      */
-    public static void handleMove(int[] newPosition, int[] oldPosition, int[][] board) {
+    public static boolean handleMove(int[] newPosition, int[] oldPosition, int[][] board) {
+        boolean legalMove = false;
         int row = newPosition[0];
         int col = newPosition[1];
-        int oldValue = board[oldPosition[0]][oldPosition[1]];
-        int newValue = oldValue + 1; // we increment the value of the snake's head by one, so that after one round of starving, it has effectively moved forward by one cell, unless it has eaten food, in which case it grows by one cell in the moving direction.
-        switch (board[row][col]) {
-            case -1: // food
-                board[row][col] = newValue; // move by one cell
-                feedSnake(board); // this offsets the starving of the snake which always takes place after moving
-                putFood(board);
-                break;
-            case 0: // field
-                board[row][col] = newValue; // move by one cell
-                break;
-            default:
-                break;
+        if (row < 0 && row >= board.length && col < 0 && col >= board[row].length) { // check bounds
+            legalMove = false;  // strictly speaking, this is not necessary, but it makes the code more readable
+        } else {
+            int oldValue = board[oldPosition[0]][oldPosition[1]];
+            int newValue = oldValue + 1; // we increment the value of the snake's head by one, so that after one round of starving, it has effectively moved forward by one cell, unless it has eaten food, in which case it grows by one cell in the moving direction.
+            switch (board[row][col]) {
+                case -1: // food
+                    board[row][col] = newValue; // move by one cell
+                    feedSnake(board); // this offsets the starving of the snake which always takes place after moving
+                    putFood(board);
+                    legalMove = true;
+                    break;
+                case 0: // field
+                    board[row][col] = newValue; // move by one cell
+                    legalMove = true;
+                    break;
+                default: // snake
+                    legalMove = false;  
+                    break;
+            }
         }
         starveSnake(board);
+        return legalMove;
     }
 
     /**
@@ -270,16 +262,15 @@ public class Snake {
      */
     public static void main(String[] args) {
 
-        int N = 9; // board size is N x N
+        int N = 9; // size of the board is N x N
 
         int[][] myBoard = new int[N][N];
         initBoard(myBoard);
         putFood(myBoard);
-        boolean validInput = true;
-        while (validInput) {
-            System.out.flush(); // clear the console
+        boolean legalMove = true;
+        while (legalMove) {
             printboard(myBoard);
-            validInput = userMove(myBoard);
+            legalMove = userMove(myBoard);
             if (checkWin(myBoard)) {
                 System.out.println("You win! :)");
                 return;
