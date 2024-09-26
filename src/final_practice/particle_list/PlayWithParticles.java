67f0ebf7b9b6f247ebc652d4d64ef6edbc4e2329
Tobias Blickhan <tobias.blickhan@gmail.com>
2024-09-26 09:55:40 -0400

    particles update

--- src/final_practice/particle_list/PlayWithParticles.java
+++ src/final_practice/particle_list/PlayWithParticles.java
@@ -2,21 +2,6 @@ package final_practice.particle_list;
 
 public class PlayWithParticles {
 
-    public static void main(String[] args) {
-        Particle[] particles = new Particle[10];
-        for (int i = 0; i < particles.length; i++) {
-            particles[i] = new Particle(Math.random(), 0); // for Coulomb
-            // particles[i] = new Particle(Math.random(), Math.random()); // for collisions
-        }
-        Simulation sim = new Simulation(particles, 0.005);
-        for (int i = 0; i < 100; i++) {
-            sim.updatePositions();
-            // sim.updateVelocities();
-            sim.updateVelocitiesCoulomb();
-            sim.sortParticles();
-            sim.printParticles();
-            System.out.println();
-        }
-    }
+    
 
 }
