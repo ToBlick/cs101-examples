5a417841c3d82b2179bb9ecdca759e74c840ad71
Tobias Blickhan <tobias.blickhan@gmail.com>
2024-09-26 09:55:12 -0400

    hello world updates

---
+++ starter_code/MyFirstJavaProgram.java
@@ -0,0 +1,5 @@
+public class MyFirstJavaProgram {
+	public static void main(String[] args) {
+		System.out.print("Welcome to Java from the command line!");
+	}
+}
---
+++ starter_code/edu/nyu/cs/tb1234/MyFirstJavaProgram.java
@@ -0,0 +1,7 @@
+package edu.nyu.cs.tb1234;
+
+public class MyFirstJavaProgram {
+	public static void main(String[] args) {
+		System.out.print("Better welcome to Java from the command line!");
+	}
+}
